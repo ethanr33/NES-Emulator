702d9637ef8e47b9194181922f1b13fe5cbf722e
ethanr33 <eroos001@ucr.edu>
2024-03-29 14:15:50 -0700

    Added helper function is_positive

--- Helpers.cpp
+++ Helpers.cpp
@@ -9,3 +9,12 @@ bool is_bit_set(int bit_index, uint8_t value) {
 uint16_t form_address(uint8_t least_significant_byte, uint8_t most_significant_byte) {
     return (most_significant_byte << 4) + least_significant_byte;
 }
+
+// We'll consider 0 to be a positive number, because the last bit isn't set
+bool is_positive(uint8_t value) {
+    if (value == 0) {
+        return true;
+    }
+
+    return value & 0x80;
+}
