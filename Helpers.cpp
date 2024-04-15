66711ddea2e9431f9e0466b95ab22cee0fe88e5f
ethanr33 <eroos001@ucr.edu>
2024-04-14 20:09:17 -0700

    Fixed form_address

--- Helpers.cpp
+++ Helpers.cpp
@@ -7,7 +7,7 @@ bool is_bit_set(int bit_index, uint8_t value) {
 }
 
 uint16_t form_address(uint8_t least_significant_byte, uint8_t most_significant_byte) {
-    return (most_significant_byte << 4) + least_significant_byte;
+    return (most_significant_byte << 8) + least_significant_byte;
 }
 
 // We'll consider 0 to be a positive number, because the last bit isn't set
